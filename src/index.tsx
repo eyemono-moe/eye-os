d8cb2f4078a6f5d39c4a82b1651c0f9a6279cf2b
detteiu8383 <ysdr83@gmail.com>
2023-08-06 03:00:49 +0900

    :sparkles: use macaron css

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
 /* @refresh reload */
+import { globalStyle } from "@macaron-css/core";
 import { render } from "solid-js/web";
 
-import "./index.css";
 import App from "./App";
 
 const root = document.getElementById("root");
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 render(() => <App />, root!);
+
+globalStyle("*", {
+  boxSizing: "border-box",
+  margin: 0,
+  padding: 0,
+});
