03484040ff6a0539691d84271bc86d78c176c02e
detteiu8383 <ysdr83@gmail.com>
2023-08-10 23:57:43 +0900

    :lipstick: styling banner

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ import { logger } from "./lib/useLog";
 
 const App: Component = () => {
   logger.log(`started eyeOS
-                      ___  ____
-    ___ _   _  ___   / _ \\/ ___|
-   / _ \\ | | |/ _ \\ | | | \\___ \\
-  |  __/ |_| |  __/ | |_| |___) |
-   \\___|\\__, |\\___|  \\___/|____/
-        |___/
+                        ___  ____
+      ___ _   _  ___   / _ \\/ ___|
+     / _ \\ | | |/ _ \\ | | | \\___ \\
+    |  __/ |_| |  __/ | |_| |___) |
+     \\___|\\__, |\\___|  \\___/|____/
+          |___/
 
-  https://github.com/detteiu8383/eye-os
+ https://github.com/detteiu8383/eye-os
+        twitter: @eyemono_moe
   `);
 
   return (
