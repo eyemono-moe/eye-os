f14e57c483265966ae499713b32f676a2473dd7b
detteiu8383 <ysdr83@gmail.com>
2023-08-15 13:07:25 +0900

    :wrench: use compression

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { macaronVitePlugin } from "@macaron-css/vite";
 import { defineConfig, splitVendorChunkPlugin } from "vite";
+import viteCompression from "vite-plugin-compression";
 import solid from "vite-plugin-solid";
 
 export default defineConfig({
-  plugins: [macaronVitePlugin(), solid(), splitVendorChunkPlugin()],
+  plugins: [
+    macaronVitePlugin(),
+    solid(),
+    splitVendorChunkPlugin(),
+    viteCompression({
+      algorithm: "brotliCompress",
+    }),
+  ],
 });
